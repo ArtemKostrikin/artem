825455c90ed366480e8150451bebb169ff940550
Artem <artemkostrikin12@gmail.com>
2022-09-24 20:10:31 +0300

    15 задача

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+WriteLine("Введите день недели от 1 до 7:");
+int d = Convert.ToInt32(ReadLine());
+string Day ="Неправильно ввели день недели";
+
+if(d==1) Day= "Понедельник";
+if(d==2) Day= "Вторник";
+if(d==3) Day= "Среда";
+if(d==4) Day= "Четверг";
+if(d==5) Day= "Пятница";
+if(d==6) Day= "суббота";
+if(d==7) Day= "Воскресенье";
+
+if (d > 5)
+{ 
+    WriteLine("Выходной");
+
+}
+else
+WriteLine(Day);
