efdb95fef70be0ef5e0e8136b418156c92d1de62
Artem <artemkostrikin12@gmail.com>
2022-09-24 19:50:49 +0300

    10 задача

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(ReadLine()!);
+
+int a0 = number%10;
+int a2 = number/100;
+int a1 = number/10;
+
+int result = a1;
+
+WriteLine(result);
